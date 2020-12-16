92ea18adf316c466fe53d9fb8b9703c0b758b024
李小小 <lixiaosong59@163.com>
2020-12-16 11:35:33 +0800

    合并分支

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const {getUuid} = require('./uuid');
 const {request, xhr} = require('./XHR');
 
+console.log('test');
+
+function getTest () {
+    return 'test';
+}
+
+console.log('fun', getTest(), '33333');
+
 module.exports = {
     getUuid,
     request,
